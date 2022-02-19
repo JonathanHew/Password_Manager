40cc14e6cfb56ee4ad7bda37607737fb737552b9
Jonathan Hew <jonathanhew@Jonathans-MacBook-Pro.local>
2022-02-19 16:03:57 +0000

    view function implemented

--- password_manager.py
+++ password_manager.py
@@ -2,7 +2,17 @@ master_pwd = input("What is the master password? ")
 
 #funtion to view paswords on manager
 def view():
-    print("You have selected view passwords.")
+    print('\n')
+    print("Your Passwords:")
+    #open the txt file in read mode
+    with open("passwords.txt", 'r') as file:
+        #loop through the txt file and print out each line
+        for line in file.readlines():
+            data = line.rstrip()
+            #split each line into 2 variables, one for account name, one for the password
+            account, password = data.split("|")
+            print("Account:", account, "| Password:", password)
+
 
 #funtion to add a new password onto the manager
 def add():
@@ -11,7 +21,7 @@ def add():
     
     #open a txt file in append mode to add to the end of the file, or create the file if it doesnt exist 
     with open("passwords.txt", 'a') as file:
-        file.write(name+ " : " + password + "\n")
+        file.write(name+ "|" + password + "\n")
 
 print("\n")
 print("Welcome to the Password Manager App")
